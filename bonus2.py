39265c5dbababcc78ccf6beb7ba2c7aad6a37892
AhmadAlhazmi75 <rregfhlty@gmail.com>
2024-06-09 22:48:07 +0300

    added more validation

--- bonus2.py
+++ bonus2.py
@@ -6,7 +6,7 @@ is_name_more_than_2_char: bool = len(user_name) > 2
 is_email_formula_valid: bool = user_email.endswith("@gmail.com")
 
 # extra validation for the email
-is_full_email_valid: bool = user_email.count("@") == 1 and user_email.count(".com") == 1 and user_email.count("-") == 0
+is_full_email_valid: bool = user_email.count("@") == 1  and user_email.count("-") == 0 and user_email.count("+") == 0
 
 if is_name_more_than_2_char and is_email_formula_valid and is_full_email_valid:
     print(f"Welcome {user_name}, you are registered with the email {user_email}")
@@ -15,4 +15,3 @@ else:
         print("the name length must be more than 2 characters, please provide a valid name.")
     if not is_full_email_valid or not is_email_formula_valid:
         print("the email is not valid , please provide a valid email .")
-    
