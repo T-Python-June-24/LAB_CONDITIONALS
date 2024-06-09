c6fc5f05c51707ff1e4773aee58ff5cdcae4d44b
D. F <ifonei@hotmail.com>
2024-06-09 14:33:26 +0300

    finished Movie recomming code

---
+++ movie.py
@@ -0,0 +1,15 @@
+movieName = "the message"
+movieRate = 3
+popularity= 72.65
+
+if movieRate >= 4 and popularity > 80:
+    print ("Highly recommended")
+elif movieRate >= 3 and popularity > 70:
+    print("I recommended it . It is good")
+elif movieRate <= 2 and popularity > 60:
+    print("You should check it out!")
+elif  movieRate <= 2 and popularity < 50:
+    print("Don't watch it. It is a waste of time")
+
+
+
