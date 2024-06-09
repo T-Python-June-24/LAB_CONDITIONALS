f7c5e4b56419307a16fac19d8b109967d18632e4
amira alrshidi <ameerah.alrashidi0111@gmail.com>
2024-06-09 14:45:32 +0300

    solution of lab conditionals

---
+++ movie_recommedation.py
@@ -0,0 +1,11 @@
+movie:str = "catch me if you can"
+rate:int = 3 # ration the move out of 5  
+popularity_score:float = 72.65
+if rate >= 4 and popularity_score > 80: 
+    print("Highly recommended")
+elif rate >= 3 and popularity_score > 70:
+    print("i recommended it . it is good ")
+elif rate <= 2 and popularity_score > 60:
+    print("You should check it out ")
+else :
+    print("Don't watch it. It is a waste of time")
