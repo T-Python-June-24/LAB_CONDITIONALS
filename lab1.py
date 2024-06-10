5459abb75579678cbde493566d1da8b88c11f424
e7gx <abalgabo@gmail.com>
2024-06-10 09:34:27 +0300

    .

--- lab1.py
+++ lab1.py
@@ -5,7 +5,7 @@ popularity_score = 72.65
 if rate >= 4 and popularity_score > 80:
     print( "Highly recommended")
 elif rate >= 3 and popularity_score > 70:
-    print( "I Recommended")
+    print( "I Recommended it")
 elif rate <= 2 and popularity_score > 60:
     print( "You should check it out!")
 else:
