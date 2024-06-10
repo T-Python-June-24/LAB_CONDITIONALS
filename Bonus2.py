677b5f798d33fab6c2b462f2d1933bcd1230575b
Sarah <monifsarah@gmail.com>
2024-06-10 07:18:44 +0300

    SolvedLAB_CONDITIONALS,Bonus 1,2

--- Bonus2.py
+++ Bonus2.py
@@ -1,8 +1,8 @@
 name:str=input("Enter your name : ")
 email:str=input("Enter your email  : ")
-if len(name)<2 :
+if len(name)<=2 :
     print("the name length must be more than 2 characters, please provide a valid name.")
-if email.count("@gmail")>1 or email.count("@gmail")==0 or not email.endswith("@gmail.com") or email.count(" ")!=0 :
+if email.count("@gmail")>1 or email.count("@gmail")==0 or not email.endswith("@gmail.com") or email.count(" ")!=0 or email.count("@")>1 :
    print("the email is not valid , please provide a valid email")
 else :
     print(f"welcome {name}  , you registered with the email{email} !")
