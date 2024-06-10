70ad0b2fa7a3e3608e84eb06ae576055722cbe7b
raynasser <rnff-333@hotmail.com>
2024-06-10 14:24:07 +0300

    update

--- DAY4_LAB2_BONUS1.py
+++ DAY4_LAB2_BONUS1.py
@@ -20,9 +20,13 @@ bmi = weight / (height ** 2)
 
 if bmi >= 25:
     print("You are overwieght you need to work out more and watch your diet")
-elif bmi >= 18.5 or bmi <= 24.9:
+# elif bmi >= 18.5 or bmi <= 24.9:
+#     print("You are fit & healthy")
+# elif bmi < 18.5:
+#     print("You are underweight. Watch your health")
+elif bmi >= 18.5:
     print("You are fit & healthy")
-elif bmi < 18.5:
+elif bmi <= 18.5:
     print("You are underweight. Watch your health")
 else:
     print("Something went wrong!")
