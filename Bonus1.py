4334c8b47bffa9866c72a9882f4afe582a81ec70
9twy <saeed.abdullah.alghamdi@outlook.sa>
2024-06-11 10:25:10 +0300

    solution updated

--- Bonus1.py
+++ Bonus1.py
@@ -4,7 +4,7 @@ BMI=float(wieght/height**2)
 print(BMI)
 if BMI <18.5:
     print("You are underweight. Watch your health.")
-elif BMI>=18.5 and BMI<=24.99:
+elif BMI >=18.5 and BMI <=24.9 :
     print("You are fit & healthy.")
 else:
     print("You are overwieght you need to work out more and watch your diet.")
--- Bonus2.py
+++ Bonus2.py
@@ -1,20 +1,14 @@
 userName=input("Please Enter Your name: ")
-ueserEmail=input("Please Enter Your Email: ")
+userEmail=input("Please Enter Your Email: ")
 stackMessages=[]
 flage=True
-if len(ueserEmail)<=2:
-    message="you need to provide a name more than 2 charcters . Please provide real name"
-    stackMessages.append(message)
-    flage=False
-print(ueserEmail.find("gmail.com"))
-if  not ueserEmail.find("@gmail.com"):
-    message="we only accept gmail emails. please try again"
-    stackMessages.append(message)
-    flage=False
+if len(userName)<=2:
+    message="you need to provide a name more than 2 charcters ."
+    print(message)
+    
+if not userEmail.endswith("@gmail.com"):
+    print("incorrect email strcure")
 
+if not len(userName)<=2 and  userEmail.endswith("@gmail.com"):
+    print("welcom to our website")
 
-if flage==True:
-    print("Welcom to our website :)")
-else:
-    for i in stackMessages:
-        print(i+"\n")
